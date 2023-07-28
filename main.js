285c745159b07e89013d3df5f6df5aa12a01ad0f
felipe benavides <felipebenavides1030@gmail.com>
2023-07-28 20:07:08 -0300

    3er commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//verion normal:
+
 // const productos = [
 //     { nombre: " Nombre: obj1 \t ", precio: "Precio: " + 100 },
 //     { nombre: " Nombre: obj2 \t ", precio: "Precio: " + 150 },
@@ -59,74 +61,151 @@
 // const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
 // alert(`El total a pagar por su compra es: ${total} \n\nVuelva Pronto!`);
 
-//=========================00
+//=========================
+//version con objeto: 
 
+// const productos = [
+//     { nombre: " Nombre: obj1 \t ", precio: "Precio: " + 100 },
+//     { nombre: " Nombre: obj2 \t ", precio: "Precio: " + 150 },
+//     { nombre: " Nombre: obj3 \t ", precio: "Precio: " + 200 },
+//     { nombre: " Nombre: obj4 \t ", precio: "Precio: " + 250 },
+// ];
+
+// function Producto (nombre, precio, unidades) {
+//     this.nombre = nombre;
+//     this.precio = precio;
+//     this.unidades = unidades;
+// }
+
+// let carrito = [];
+// let seleccion = prompt("Bienvenido a nuestra tienda!\nDesea comprar algo? (si / no)");
+// while (seleccion != "si" && seleccion != "no") {
+//     alert("Por favor ingresar si o no");
+//     seleccion = prompt("Desea comprar algun producto ? (si o no)");
+// } 
+
+// if (seleccion == "si") {
+//     alert("Aqui nuestros productos: ");
+//     let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+//     alert(todosLosProductos.join("\n"));
+// } else if (seleccion == "no") {
+//     alert("Gracias por visitarnos !");
+// }
+
+// while (seleccion != "no") {
+//     let producto = prompt("Ingresar el nombre del producto que dese llevar: ");
+//     let precio = 0;
+
+//     if (producto == "obj1" || producto == "obj2" || producto == "obj3" || producto == "obj4") {
+//         switch (producto) {
+//             case "obj1":
+//                 precio = 100;
+//                 break;
+//             case "obj2":
+//                 precio = 150;
+//                 break;
+//             case "obj3":
+//                 precio = 200;
+//                 break;
+//             case "obj4":
+//                 precio = 250;
+//                 break;
+//             default:
+//                 break;
+//         }
+//         let unidades = parseInt(prompt("Igrese la cantidad que desea llevar: "));
+
+//         carrito.push(new Producto(producto, unidades, precio));
+
+//     } else {
+//         alert("No hay stock para ese producto :/")
+//     }
+//     seleccion = prompt("Desea seguir comprando? (si o no)");
+//     while (seleccion === "no") {
+//         alert("Gracias por la compra, aqui su pedido: ");
+//         carrito.map((carritoFinal) => {
+//             alert(`Producto: ${carritoFinal.nombre}, \n\nUnidades: ${carritoFinal.unidades}, \n\nTotal a Pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
+//         });
+//         break;
+//     }
+// }
+
+// const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+// alert(`El total a pagar por su compra es: ${total} \n\nVuelva Pronto!`);
+
+
+//============================
+//version con objetos (variables renombradas):
 
 const productos = [
-    { nombre: " Nombre: obj1 \t ", precio: "Precio: " + 100 },
-    { nombre: " Nombre: obj2 \t ", precio: "Precio: " + 150 },
-    { nombre: " Nombre: obj3 \t ", precio: "Precio: " + 200 },
-    { nombre: " Nombre: obj4 \t ", precio: "Precio: " + 250 },
-];
-
-function Producto (nombre, precio, unidades) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.unidades = unidades;
+        { nombre: " Nombre: obj1 \t ", precio: "Precio: " + 100 },
+        { nombre: " Nombre: obj2 \t ", precio: "Precio: " + 150 },
+        { nombre: " Nombre: obj3 \t ", precio: "Precio: " + 200 },
+        { nombre: " Nombre: obj4 \t ", precio: "Precio: " + 250 },
+    ];
+    
+    class Producto {
+    constructor(nombre, precio, unidades) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.unidades = unidades;
+    }
 }
-
-let carrito = [];
-let seleccion = prompt("Bienvenido a nuestra tienda!\nDesea comprar algo? (si / no)");
-while (seleccion != "si" && seleccion != "no") {
-    alert("Por favor ingresar si o no");
-    seleccion = prompt("Desea comprar algun producto ? (si o no)");
-} 
-
-if (seleccion == "si") {
-    alert("Aqui nuestros productos: ");
-    let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todosLosProductos.join("\n"));
-} else if (seleccion == "no") {
-    alert("Gracias por visitarnos !");
-}
-
-while (seleccion != "no") {
-    let producto = prompt("Ingresar el nombre del producto que dese llevar: ");
-    let precio = 0;
-
-    if (producto == "obj1" || producto == "obj2" || producto == "obj3" || producto == "obj4") {
-        switch (producto) {
-            case "obj1":
-                precio = 100;
-                break;
-            case "obj2":
-                precio = 150;
-                break;
-            case "obj3":
-                precio = 200;
-                break;
-            case "obj4":
-                precio = 250;
-                break;
-            default:
-                break;
+    
+    let changuito = [];
+    let opcion = prompt("Bienvenido a nuestra tienda!\nDesea comprar algo? (si / no)");
+    while (opcion != "si" && opcion != "no") {
+        alert("Por favor ingresar si o no");
+        opcion = prompt("Desea comprar algun producto ? (si / no)");
+    } 
+    
+    if (opcion == "si") {
+        alert("Aqui nuestros productos: ");
+        let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+        alert(todosLosProductos.join("\n"));
+    } else if (opcion == "no") {
+        alert("Gracias por visitarnos !");
+    }
+    
+    while (opcion != "no") {
+        let producto = prompt("Ingresar el nombre del producto que dese llevar: ");
+        let precio = 0;
+    
+        if (producto == "obj1" || producto == "obj2" || producto == "obj3" || producto == "obj4") {
+            switch (producto) {
+                case "obj1":
+                    precio = 100;
+                    break;
+                case "obj2":
+                    precio = 150;
+                    break;
+                case "obj3":
+                    precio = 200;
+                    break;
+                case "obj4":
+                    precio = 250;
+                    break;
+                default:
+                    break;
+            }
+            let unidades = parseInt(prompt("Igrese la cantidad que desea llevar: "));
+    
+            changuito.push(new Producto(producto, unidades, precio));
+    
+        } else {
+            alert("No hay stock para ese producto :/")
         }
-        let unidades = parseInt(prompt("Igrese la cantidad que desea llevar: "));
-
-        carrito.push(new Producto(producto, unidades, precio));
-
-    } else {
-        alert("No hay stock para ese producto :/")
+        opcion = prompt("Desea seguir comprando? (si o no)");
+        while (opcion === "no") {
+            alert("Gracias por la compra, aqui su pedido: ");
+            changuito.map((changuitoTotal) => {
+                alert(`Producto: ${changuitoTotal.nombre}, \n\nUnidades: ${changuitoTotal.unidades}, \n\nTotal a Pagar: ${changuitoTotal.unidades * changuitoTotal.precio}`)
+            });
+            break;
+        }
     }
-    seleccion = prompt("Desea seguir comprando? (si o no)");
-    while (seleccion === "no") {
-        alert("Gracias por la compra, aqui su pedido: ");
-        carrito.map((carritoFinal) => {
-            alert(`Producto: ${carritoFinal.nombre}, \n\nUnidades: ${carritoFinal.unidades}, \n\nTotal a Pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
-        });
-        break;
-    }
-}
+    
+    const total = changuito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+    alert(`El total a pagar por su compra es: ${total} \n\nVuelva Pronto!`);
 
-const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-alert(`El total a pagar por su compra es: ${total} \n\nVuelva Pronto!`);
+
